281e63d67fa4163a05b2c09b41c0f2c0ef50202a
tangyue <tangyue1004@gmail.com>
2012-03-29 17:33:43 +0800

    game loading

--- src/tangyue/circlebreaker/view/GameActivity.java
+++ src/tangyue/circlebreaker/view/GameActivity.java
@@ -3,22 +3,55 @@ package tangyue.circlebreaker.view;
 import tangyue.circlebreaker.R;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.WindowManager;
 
 public class GameActivity extends BaseActivity {
 	private BreakerView breakerview;
 	private boolean isPause = false;
 	private PauseDialog pauseDialog;
+	private Handler handler;
+	private Thread loadingThread;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		int level = getLevel();
-		breakerview = new BreakerView(this, level);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
 				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		setContentView(breakerview);
-		pauseDialog = new PauseDialog(this, R.style.fullscreen);
+		loadGame();
+	}
+
+	public void loadGame() {
+		handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				switch (msg.what) {
+				case 1:
+					loadingThread = null;
+					int level = getLevel();
+					breakerview = new BreakerView(GameActivity.this, level);
+					pauseDialog = new PauseDialog(GameActivity.this,
+							R.style.fullscreen);
+					setContentView(breakerview);
+					break;
+				default:
+					break;
+				}
+			}
+		};
+		loadingThread = new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(10);
+					handler.sendEmptyMessage(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		loadingThread.start();
 	}
 
 	public void startLevelComplete(int score, int level) {
